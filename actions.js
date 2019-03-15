a88792e6cbec1d33542d20d1fbded88e9fe549be
Pawel Oczkowski <pawel.oczko84@gmail.com>
2019-03-15 09:57:50 +0100

    Add reducer

--- actions.js
+++ actions.js
@@ -1,15 +1,23 @@
 import uuid from uuid;
 
+export const ADD_COMMENT = 'ADD_COMMENT'
+export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT'
+
 function addComment(text) {
     return {
-        type: 'ADD_COMMENT',
+        type: ADD_COMMENT,
         text,
-        id: uuid.v4()
+        id: uuid.v4(),
+        thumbs_up: 0,
+        thumbs_down: 0
     }
 };
  function editComment(text,id) {
      return {
-         type: 'EDIT_COMMENT',
+         type: EDIT_COMMENT,
          id,
          text
      }
@@ -17,24 +25,24 @@ function addComment(text) {
 
  function removeComment(id) {
      return {
-         type: 'REMOVE_COMMENT',
+         type: REMOVE_COMMENT,
          id
      }
  };
 
 function thumbDownComment (id) {
     return {
-        type: 'THUMB_DOWN_COMMENT',
+        type: THUMB_DOWN_COMMENT,
         id,
-        vote: vote - 1
+        thumbs_down: thumbs_down - 1
     }
 };
 
 function thumbUpComment (id) {
     return {
-        type: 'THUMB_UP_COMMENT',
+        type: THUMB_UP_COMMENT,
         id,
-        vote: vote + 1
+        thumbs_up: thumbs_up + 1
     }
 }
 
